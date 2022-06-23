d00a0d7b98a4cf3a428c443994e287bb875112d5
Paul Annesley <paul@annesley.cc>
2022-06-24 09:25:15 +0930

    solargraph (ruby) enabled in lspconfig

--- init.lua
+++ init.lua
@@ -90,7 +90,7 @@ vim.cmd [[
 
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-for _, server in ipairs({ "gopls", "rust_analyzer", "yamlls" }) do
+for _, server in ipairs({ "gopls", "rust_analyzer", "yamlls", "solargraph" }) do
   lspconfig[server].setup { capabilities = capabilities }
 end
 
