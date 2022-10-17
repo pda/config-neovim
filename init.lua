e23ccc371333ee91c6910cb4ddd9fefd8baa9513
Paul Annesley <paul@annesley.cc>
2022-10-17 13:46:35 +1030

    cmp_nvim_lsp: update_capabilities → default_capabilities (deprecated)
    Based on this warning:
    
    > cmp_nvim_lsp.update_capabilities is deprecated, use cmp_nvim_lsp.default_capabilities instead. See :h deprecated
    > This function will be removed in cmp-nvim-lsp version 1.0.0
    > stack traceback:
    >         ...im/pack/pda/start/cmp-nvim-lsp/lua/cmp_nvim_lsp/init.lua:68: in function 'update_capabilities'
    >         /Users/pda/.config/nvim/init.lua:99: in main chunk

--- init.lua
+++ init.lua
@@ -96,7 +96,7 @@ vim.g.terraform_fmt_on_save = true
 -- plugins
 
 local lspconfig = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 for _, server in ipairs({ "gopls", "rust_analyzer", "yamlls", "solargraph" }) do
   lspconfig[server].setup { capabilities = capabilities }
 end
