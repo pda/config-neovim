e19e5da64d484ec0515aece4e80309a03e5d22ab
Paul Annesley <paul@annesley.cc>
2024-08-08 21:28:48 +0930

    s/ruby_ls/ruby_lsp/ I guess it changed upstream to match ruby-lsp name

--- init.lua
+++ init.lua
@@ -476,7 +476,7 @@ require('lazy').setup({
         },
         julials = {},
         -- pyright = {},
-        ruby_ls = {},
+        ruby_lsp = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
