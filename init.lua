5e875b789b9a9b3473a010649ac4b5a24a00cccc
Paul Annesley <paul@annesley.cc>
2025-03-06 22:16:46 +1030

    Go: organizeImports on save
    Turns out this is a bit of a mess in lspconfig land:
    https://github.com/neovim/nvim-lspconfig/issues/115

--- init.lua
+++ init.lua
@@ -122,6 +122,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    pcall(function()
+      vim.lsp.buf.code_action { context = { only = { 'source.organizeImports' }, diagnostics = {} }, apply = true }
+      vim.lsp.buf.format()
+    end)
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
